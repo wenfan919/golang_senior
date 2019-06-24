1077d90f507756996d3a0eee5067e38fd3d79498
yanyong <yanyongc@yonyou.com>
2019-06-24 09:56:12 +0800

    update the readinput

---
+++ file/readinput1.go
@@ -0,0 +1,23 @@
+package file
+
+
+import "fmt"
+
+var (
+	firstName, lastName, s string
+	i int
+	f float32
+	input = "56.12 / 5212 / Go"
+	format = "%f / %d / %s"
+)
+
+func Test() {
+	fmt.Println("Please enter your full name: ")
+	fmt.Scanln(&firstName, &lastName)
+	// fmt.Scanf("%s %s", &firstName, &lastName)
+	fmt.Printf("Hi %s!\n", lastName) // Hi Chris Naegels
+	fmt.Printf("Hi %s!\n", firstName) // Hi Chris Naegels
+	fmt.Sscanf(input, format, &f, &i, &s)
+	fmt.Println("From the string we read: ", f, i, s)
+	// 输出结果: From the string we read: 56.12 5212 Go
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "golang_senior/file"
+
+func main(){
+
+	file.Test()
+
+}
